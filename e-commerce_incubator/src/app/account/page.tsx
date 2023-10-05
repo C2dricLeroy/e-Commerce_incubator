d0caf33819811cf9825d3d2441ae3cd7af8b7afe
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-10-05 17:59:13 +0100

    feat(account): add account page and components

--- e-commerce_incubator/src/app/account/page.tsx
+++ e-commerce_incubator/src/app/account/page.tsx
@@ -2,11 +2,26 @@
 import Header from '@/app/components/Header/Header.tsx';
 // @ts-ignore
 import Footer from '@/app/components/footer/Footer.tsx';
+// @ts-ignore
+import AccountComponent from '@/app/components/accountComponent/AccountComponent.tsx';
+// @ts-ignore
+import SavedCart from '@/app/components/savedCart/SavedCart.tsx';
+import styles from './style.module.css';
 
 export default function Account() {
   return (
-        <div>
+        <div className={styles.pageContainer}>
             <Header></Header>
+            <div >
+                <div className={styles.container}>
+                    <h1>Mon compte</h1>
+                    <div className={styles.accountContainer}>
+                        <SavedCart></SavedCart>
+                        <AccountComponent></AccountComponent>
+                    </div>
+                </div>
+            </div>
+
             <Footer></Footer>
         </div>
   );
---
+++ e-commerce_incubator/src/app/components/savedCart/SavedCart.tsx
@@ -0,0 +1,7 @@
+export default function SavedCart() {
+  return (
+      <div>
+          <h1>Saved Cart</h1>
+      </div>
+  );
+}
