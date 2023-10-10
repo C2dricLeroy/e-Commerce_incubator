5f4d575e71c30c6f92c6fe935672e046e22d3b9f
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-10-10 15:15:30 +0100

    feat(cart): display and stylize components

--- e-commerce_incubator/src/app/components/cart/Cart.tsx
+++ e-commerce_incubator/src/app/components/cart/Cart.tsx
@@ -2,13 +2,47 @@
 
 // @ts-ignore
 import useCartViewModel from '@/viewmodels/CartViewModel.ts';
+import { FaPlus, FaMinus } from 'react-icons/fa';
+import styles from './style.module.css';
 
 export default function Cart() {
   const cartViewModel = useCartViewModel();
 
   return (
-                <div>
-                        <h1>Cart</h1>
-                </div>
+      <div>
+          <div className={styles.myCart}>
+              <div className={styles.cartContainer}>
+                  {cartViewModel.cartProducts && cartViewModel.cartProducts.map((item: any) => (
+                      <div className={styles.card} key={item.name}>
+                          <div className={styles.product}>
+                              <img className={styles.productImage} src={`/products_images/${item.image_path}`} alt={`${item.name} image`} />
+                              <p className={styles.productTitle}>{item.name}</p>
+                              <button>
+                                  <FaMinus />
+                              </button>
+                              <p className={styles.quantity}>{item.quantity}</p>
+                              <button>
+                                  <FaPlus />
+                              </button>
+                              <button>Supprimer</button>
+                          </div>
+                      </div>
+                  ))}
+              </div>
+              <div className={styles.total}>
+                  <div className={styles.totalText}>
+                      <p>Total des articles</p>
+                      <p>Livraison</p>
+                      <p>Total</p>
+                  </div>
+                  <div className={styles.paymentButton}>
+                      <button>Commander</button>
+                  </div>
+                  <div className={styles.saveCart}>
+                      <button>Sauvegarder ce panier</button>
+                  </div>
+              </div>
+          </div>
+      </div>
   );
 }
--- e-commerce_incubator/src/viewmodels/CartViewModel.ts
+++ e-commerce_incubator/src/viewmodels/CartViewModel.ts
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
+// @ts-ignore
+import Products from '@/models/Products.ts';
 
 export default function useCartViewModel() {
   const [cart, setCart] = useState([]);
+  const [cartProducts, setCartProducts] = useState<Products[]>([]);
 
   useEffect(() => {
     const fetchCart = async () => {
@@ -16,6 +19,19 @@ export default function useCartViewModel() {
       }
     };
 
+    const fetchCartProduct = async () => {
+      const products = [];
+      for (const product of cart) {
+        const { id, quantity } = product;
+        const response = await Products.getOne(id);
+        const productWithQuantity = { ...response, quantity };
+        products.push(productWithQuantity);
+      }
+      setCartProducts(products);
+      console.log(products);
+    };
+
+    fetchCartProduct();
     fetchCart();
   }, [cart]);
 
@@ -36,5 +52,6 @@ export default function useCartViewModel() {
     clearCart,
     calculateTotalPrice,
     removeFromCart,
+    cartProducts,
   };
 }
